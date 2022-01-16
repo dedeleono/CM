1620524c99e03dd921514736364557f5b01f7c83
Diego Oliva <diegooliva@diegos-air.lan>
2022-01-16 14:21:58 -0600

    >

--- src/App.svelte
+++ src/App.svelte
@@ -167,7 +167,7 @@
       <br />
       <!-- Main Body -->
       <div class="p-6">
-        <img src={IMAGE_LINK} alt="" class=" w-9/12 h-fit mx-auto m-5" />
+        <img src='./images/bg.png' alt="logo" class="w-9/12 h-fit mx-auto m-5" />
         <div class="text-sm sm:text-md font-semibold pb-5 text-gray-600 desc">
           {DESCRTIPTION}
         </div>
